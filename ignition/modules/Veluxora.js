5c8972a84df21c7b50d6c973b0576564184b878f
yebology <139052533+yebology@users.noreply.github.com>
2025-06-03 16:37:29 +0700

    prepare to deploy

--- ignition/modules/Veluxora.js
+++ ignition/modules/Veluxora.js
@@ -1,13 +1,19 @@
 // This setup uses Hardhat Ignition to manage smart contract deployments.
 // Learn more about it at https://hardhat.org/ignition
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    optimism: {
-      url: `https://`,
-      accounts: []
-    }
-  }
-}
+    holesky: {
+      url: `https://ethereum-holesky.publicnode.com`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      holesky: `${process.env.ETHERSCAN_API_KEY}`,
+    },
+  },
+};
